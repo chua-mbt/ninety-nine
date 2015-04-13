a557e05e8ccddfa4326f80c8ffd89f91bff4f988
Michael Chua <chua.mbt@gmail.com>
2015-04-13 18:54:32 +0000

    Problem 17

---
+++ src/main/scala/ninety/nine/P17.scala
@@ -0,0 +1,30 @@
+package ninety.nine
+
+object P17 extends Problem[(List[Any], List[Any])] {
+  val NAME = "P17"
+  val DESCRIPTION = "split a list into two parts"
+  val TESTCASE = List(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  val TESTOUT = (List('a, 'b, 'c), List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+
+  def recursiveSolution(input: Any*): (List[Any], List[Any]) = {
+    val n = input(0).asInstanceOf[Int]
+    val list = input(1).asInstanceOf[List[Any]]
+    recursiveSplit(n, list)
+  }
+
+  def idiomaticSolution(input: Any*): (List[Any], List[Any]) = {
+    val n = input(0).asInstanceOf[Int]
+    val list = input(1).asInstanceOf[List[Any]]
+    list.splitAt(n)
+  }
+
+  def recursiveSplit(n: Int, list: List[Any], pre: List[Any] = List.empty[Any]): (List[Any], List[Any]) =
+    list match {
+      case Nil => (pre, Nil)
+      case head::tail => if(P04.recursiveLength(pre) == n){
+        (pre, head::tail)
+      } else {
+        recursiveSplit(n, tail, pre:+head)
+      }
+    }
+}
--- src/test/scala/NinetyNineSpec.scala
+++ src/test/scala/NinetyNineSpec.scala
@@ -6,7 +6,7 @@ class NinetyNineSpec extends FlatSpec with ShouldMatchers {
   val problems = List(
     // List Problems
     P01, P02, P03, P04, P05, P06, P07, P08, P09, P10,
-    P11, P12, P13, P14, P15, P16
+    P11, P12, P13, P14, P15, P16, P17
     // Arithmetic Problems
   )
   problems.foreach { problem =>
